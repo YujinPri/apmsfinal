4ffb2c3ccd984bd344c54ce907ae9d7dd1f157e1
Hayme <jamesmatthewbelgica@gmail.com>
2023-09-07 00:32:53 +0800

    Added Database.py

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from config import URL_DATABASE
+
+engine = create_engine(URL_DATABASE)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
+
+
+def getDB():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
