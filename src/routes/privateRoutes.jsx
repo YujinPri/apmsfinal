a94cf4dbf52b61ba54f9ecc6103c9bcade72fcd4
bukangliwayway <jamesmatthewbelgica@gmail.com>
2023-09-21 05:07:08 +0800

    arranged the files

--- src/routes/privateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
-
-const PrivateRoutes = () => {
-  const { token } = useAuth();
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoutes;
