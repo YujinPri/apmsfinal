58a1bdadfb62305e99897d554848c9a6d9179370
bukangliwayway <jamesmatthewbelgica@gmail.com>
2023-09-24 01:53:15 +0800

    deleted old backend

--- backend/config.py
+++
@@ -1,5 +0,0 @@
-URL_DATABASE = "postgresql://postgres:TKJhmMLXESDlVOJVsTK6@containers-us-west-129.railway.app:7713/railway"
-SECRET_KEY = "JPqU94zhU3FyDDqRUEOS4Nrr3XKfY3apkAWRjXNxGGn2SzRlIs036wyCRClYYpRJ"
-ALGORITHM = "HS256"
-
-
